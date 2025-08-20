c49186b186859cc9236b84bb2bf73a0836f35ae0
K-K-0 <aadityyadav7531@gmail.com>
2025-08-20 11:16:17 +0530

    Maximum Nesting Depth of the Parentheses

---
+++ DSA/MaxParanthesis.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+
+        result = 0
+        current = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                current += 1
+            elif s[i] == ")": 
+                current -= 1
+            result = max(result, current)
+        return result
+        
+
+        
